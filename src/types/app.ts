0708bf802a13d121771192fe7086a374b094029f
Luiz F Monge <luizfilipemonge@outlook.com>
2025-10-19 00:49:41 -0300

    Add Application interface to define app structure and properties

---
+++ src/types/app.ts
@@ -0,0 +1,21 @@
+export interface Application {
+  id: string;
+  name: string;
+  icon: string | preact.JSX.Element;
+  component: preact.JSX.Element;
+  title: string;
+  i18n?: Record<string, { appName?: string }>;
+  resizable?: boolean;
+  maximizable?: boolean;
+  minimizable?: boolean;
+  closable?: boolean;
+  singleInstance?: boolean;
+  width?: number;
+  height?: number;
+  x?: number;
+  y?: number;
+  startMenuApp?: boolean;
+  desktopApp?: boolean;
+  instanceId?: string;
+  minimized?: boolean;
+}
