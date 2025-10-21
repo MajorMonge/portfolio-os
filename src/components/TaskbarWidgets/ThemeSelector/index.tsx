8ac811cc744675118bf6064c3f8d33f26fe6d5fc
Luiz F Monge <luizfilipemonge@outlook.com>
2025-10-21 13:55:18 -0300

    Refactor ThemeSelector to utilize useDropdownPosition for dynamic dropdown classes and tooltip handling

--- src/components/TaskbarWidgets/ThemeSelector/index.tsx
+++ src/components/TaskbarWidgets/ThemeSelector/index.tsx
@@ -1,20 +1,28 @@
 import { h } from "preact";
 import { $themeStore, setTheme, themes } from "@/store/ThemeStore";
-import { useEffect } from "preact/hooks";
 import { useStore } from "@nanostores/preact";
 import { useTranslation } from "@/i18n";
+import { $taskbarStore } from "@/store/TaskbarStore";
+import { getTooltipClasses } from "@/helpers/getPosition";
+import { useDropdownPosition } from "@/helpers/hooks/useDropdownPosition";
 
 export default function ThemeSelector() {
   const { t } = useTranslation();
 
   const activeTheme = useStore($themeStore);
+  const taskbarOptions = useStore($taskbarStore);
+  const currentPosition = taskbarOptions.position;
+  const { buttonRef, dropdownClasses } = useDropdownPosition(currentPosition);
 
   return (
-    <div className="dropdown dropdown-top flex align-center justify-center">
+    <div
+      className={`${dropdownClasses} flex align-center justify-center`}
+    >
       <div
+        ref={buttonRef}
         tabIndex={0}
         role="button"
-        className="btn btn-ghost btn-square my-auto tooltip"
+        className={`btn btn-ghost btn-square my-auto ${getTooltipClasses(currentPosition)}`}
         data-tip={t("ui.taskbar.themeSelector.tooltip")}
       >
         🎨
