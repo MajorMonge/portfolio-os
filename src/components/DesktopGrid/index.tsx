8186c82c9a78c6be2e7b429d236c4f9d9e6e570f
Luiz F Monge <luizfilipemonge@outlook.com>
2025-10-21 13:28:33 -0300

    Refactor DesktopGrid component to set default taskbarFit value and improve position class handling

--- src/components/DesktopGrid/index.tsx
+++ src/components/DesktopGrid/index.tsx
@@ -7,24 +7,23 @@ interface DesktopGridProps {
   taskbarFit?: "left" | "right" | "bottom" | "top";
 }
 
-export default function DesktopGrid({ apps, taskbarFit }: DesktopGridProps) {
+export default function DesktopGrid({ apps, taskbarFit = "bottom" }: DesktopGridProps) {
   const getPositionClasses = () => {
     switch (taskbarFit) {
       case "top":
-        return "top-0 left-0 w-full h-[calc(100%-4.75rem)]";
+        return "pt-[4.75rem] pb-0 pl-0 pr-0 top-0 left-0 w-full h-full";
       case "left":
-        return "left-0 top-0 h-full w-[calc(100%-4.75rem)]";
+        return "pt-0 pb-0 pl-[4.75rem] pr-0 left-0 top-0 h-full w-full";
       case "right":
-        return "right-0 top-0 h-full w-[calc(100%-4.75rem)]";
+        return "pt-0 pb-0 pl-0 pr-[4.75rem] right-0 top-0 h-full w-full";
       case "bottom":
-        return "bottom-0 left-0 w-full h-[calc(100%-4.75rem)]";
       default:
-        return "bottom-0 left-0 w-full h-[calc(100%-4.75rem)]";
+        return "pt-0 pb-[4.75rem] pl-0 pr-0 bottom-0 left-0 w-full h-full";
     }
   };
 
   return (
-    <div className={`w-full h-full ${getPositionClasses()} flex  flex-row`}>
+    <div className={`absolute ${getPositionClasses()} flex flex-row`}>
       <div
         className={`w-full h-full p-4 grid grid-rows-[repeat(auto-fill,minmax(7rem,1fr))] auto-cols-max gap-2 grid-flow-col content-start justify-items-start`}
       >
