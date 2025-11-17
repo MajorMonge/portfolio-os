55890237f543292f53ac67a25f4547dd321c98ed
Luiz F Monge <luizfilipemonge@outlook.com>
2025-11-17 02:12:23 -0300

    Refactor ContentRenderer to utilize MDRendererComponent for improved content rendering

--- src/components/ContentRenderer/index.tsx
+++ src/components/ContentRenderer/index.tsx
@@ -1,16 +1,23 @@
 import type { RichTextItemResponse } from "@notionhq/client";
 import { h } from "preact";
+import MDRendererComponent from "../MDRenderer";
 
 interface ContentRendererProps {
   content: string | RichTextItemResponse;
 }
 
 export default function ContentRenderer({ content }: ContentRendererProps) {
-  if (typeof content !== "string" && content.href !== null && content.href !== undefined) {
+  if (
+    typeof content !== "string" &&
+    content.href !== null &&
+    content.href !== undefined
+  ) {
     return <iframe src={content.href} class="w-full h-full border-none" />;
+  } else {
+    return (
+      <MDRendererComponent
+        content={typeof content === "string" ? content : content.plain_text}
+      />
+    );
   }
-
-  return (
-    <div>{typeof content === "string" ? content : content.plain_text}</div>
-  );
 }
